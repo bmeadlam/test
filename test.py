b8da19c5c64939b72334182b7858d457f2f9d211
bmeadlam <thethousandthman@mac.com>
2018-01-27 14:00:31 -0500

    zeros in array

--- test.py
+++ test.py
@@ -1,30 +1,20 @@
-
-
-def rot_cyc(mat, i, j, n):
-    dum = mat[i][j]
-    for _ in range(3):
-        mat[i][j] = mat[n-1-j][i]
-        i, j = n-1-j, i
-    mat[i][j] = dum
-    return mat
-
-
-def rot_mat(mat):
-    n = len(mat)
-    for i in range(int(n/2)):
-        for j in range(i, n-i-1):
-            mat = rot_cyc(mat, i, j, n)
-    return mat
-
-
-def print_mat(mat):
-    for row in mat:
-        print(row)
-    print('\n')
-
-
-mat = []
-for i in range(10):
-    mat.append([1 + j + 10*i for j in range(10)])
-print_mat(mat)
-print_mat(rot_mat(mat))
+class Solution:
+    # @param A : list of list of integers
+    # @return the same list modified
+    def setZeroes(self, A):
+        if len(A) == 0:
+            return
+        c_z = {}
+        r_z = {}
+        for i, row in enumerate(A):
+            for j, el in enumerate(row):
+                if el == 0:
+                    r_z[i] = True
+                    c_z[j] = True
+        for i in r_z.keys():
+            for k in range(len(A[0])):
+                A[i][k] = 0
+        for j in c_z.keys():
+            for k in range(len(A)):
+                A[k][j] = 0
+        return A
