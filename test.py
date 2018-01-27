242c0b602ec00b60ab31d6e9330a04afc020276c
bmeadlam <thethousandthman@mac.com>
2018-01-27 13:15:23 -0500

    rotate a matrix

--- test.py
+++ test.py
@@ -1,18 +1,30 @@
 
 
-def uni_char_q(s):
-    char_hash = {}
-
-    for c in s:
-        try:
-            char_hash[c]
-            return True
-        except:
-            char_hash[c] = 1
-
-    return False
+def rot_cyc(mat, i, j, n):
+    dum = mat[i][j]
+    for _ in range(3):
+        mat[i][j] = mat[n-1-j][i]
+        i, j = n-1-j, i
+    mat[i][j] = dum
+    return mat
 
 
-print(uni_char_q(''))
-print(uni_char_q('abc'))
-print(uni_char_q('abcdefga'))
+def rot_mat(mat):
+    n = len(mat)
+    for i in range(int(n/2)):
+        for j in range(i, n-i-1):
+            mat = rot_cyc(mat, i, j, n)
+    return mat
+
+
+def print_mat(mat):
+    for row in mat:
+        print(row)
+    print('\n')
+
+
+mat = []
+for i in range(10):
+    mat.append([1 + j + 10*i for j in range(10)])
+print_mat(mat)
+print_mat(rot_mat(mat))
