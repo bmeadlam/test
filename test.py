473108baf0f04dad17f3d4d96ae69c36440e53bc
bmeadlam <thethousandthman@mac.com>
2018-01-28 20:46:01 -0500

    Tour of the hyercube

--- test.py
+++ test.py
@@ -1,34 +1,24 @@
-def pal(A):
-    if len(A) == 1:
-        return [[A]]
-    # print(A[-1])
-    # print(A[:-1])
-    return merge(A[-1], pal(A[:-1]))
+class Solution:
+    # @param A : integer
+    # @return a list of integers
+    def grayCode(self, A):
+        walker = 0
+        out = [walker]
+        for delta in range(1, 2**A):
+            walker = walker ^ (delta % 2**func(delta))
+            walker = walker % 2**A
+            out.append(walker)
+        return out
 
 
-def merge(ch, s):
-    out = []
-    for pal_list in s:
-        for i in range(len(pal_list)):
-            if is_pal(sum_list(pal_list[-i:]) + ch):
-                out.append(pal_list[:-i] + [sum_list(pal_list[-i:]) + ch])
-        out.append(pal_list + [ch])
-    return out
+def func(delta):
+    cnt = 0
+    for bit in reversed(bin(delta)):
+        if bit == '0':
+            cnt += 1
+        else:
+            return cnt + 1
 
 
-def is_pal(s):
-    for i in range(int(len(s)/2)):
-        if s[i] != s[-1-i]:
-            return False
-    return True
-
-
-def sum_list(l):
-    out = ''
-    for x in l:
-        out = out + x
-    return out
-
-
-# print(pal('efe'))
-print(pal('efe'))
+sol = Solution()
+print(sol.grayCode(10))
